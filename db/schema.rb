313f7e8889639a9378e4e24f05057b05c6ce739c
saraheig <sarah.composto@heig-vd.ch>
2018-04-25 08:40:13 +0000

    Add User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_24_142909) do
+ActiveRecord::Schema.define(version: 2018_04_25_083214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2018_04_24_142909) do
     t.boolean "legislative"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "pseudo"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pseudo"], name: "index_users_on_pseudo", unique: true
   end
 
 end
