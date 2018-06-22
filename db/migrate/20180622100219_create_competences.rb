efad31bab7a3ba871c0e3087036ab53839f0c51c
saraheig <sarah.composto@heig-vd.ch>
2018-06-22 10:12:12 +0000

    Run migration

--- db/migrate/20180622100219_create_competences.rb
+++ db/migrate/20180622100219_create_competences.rb
@@ -1,9 +1,11 @@
 class CreateCompetences < ActiveRecord::Migration[5.2]
   def change
     create_table :competences do |t|
-      t.string :name
+      t.string :name, limit: 250
 
       t.timestamps
     end
+
+    add_index :competences, :name, :unique => true
   end
 end
