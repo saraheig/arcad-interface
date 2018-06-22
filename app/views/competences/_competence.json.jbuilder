3df6b58f7a4e3ea84843e3ace3f016b73f0d8d07
saraheig <sarah.composto@heig-vd.ch>
2018-06-22 10:02:35 +0000

    Generate Competence

---
+++ app/views/competences/_competence.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! competence, :id, :name, :created_at, :updated_at
+json.url competence_url(competence, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :competences
   root to: redirect('/tables')
 
   resource :session, only: %i[new create destroy]
